fe2343f1de33e626e32ed7307c2df41a73049911
Hasibul Chowdhury <hasibulc91@gmail.com>
2019-10-29 20:13:45 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+class School
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def roster
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    if @roster[grade]
+      @roster[grade] << student_name
+      return @roster
+    else
+      @roster[grade] = [student_name]
+      return @roster
+    end
+  end
+  
+end
